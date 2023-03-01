adf6fec8fec8dbf26ef22b384fa6da63ba33602f
Miguel Camarena <migcam@Miguels-MacBook-Air.local>
2023-03-01 16:50:55 -0400

    get list of rows

--- index.js
+++ index.js
@@ -5,24 +5,30 @@ import puppeteer from 'puppeteer';
     const rnc = '130334269'
     const url = 'https://www.dgii.gov.do/app/WebApps/ConsultasWeb/consultas/rnc.aspx'
     const button_Id = '#ctl00_cphMain_btnBuscarPorRNC'
-    const companySelector = '#ctl00_cphMain_dvDatosContribuyentes > tbody > tr:nth-child(3) > td:nth-child(2)'
+    const company_td_Selector = '#ctl00_cphMain_dvDatosContribuyentes > tbody > tr:nth-child(3) > td:nth-child(2)'
 
     const browser = await puppeteer.launch(
-        // {
-        //     headless: false,
-        //     slowMo: 250
-        // }
-        );
+        {
+            headless: false,
+            slowMo: 250
+        });
 
     const page = await browser.newPage();
     await page.goto(url);
     await page.type(rnc_input_id,rnc)
     await page.click(button_Id)
-    
-    await page.waitForSelector(companySelector)
-    let element = await page.$(companySelector)
-    let value = await page.evaluate(el => el.textContent, element)
-    console.log(value)
+
+    await page.waitForSelector(company_td_Selector)
+
+    const data = await page.$$eval('#ctl00_cphMain_dvDatosContribuyentes tbody tr', trs => trs.map((tr) => {
+        return {
+           column:  tr.cells.item(0).innerText,
+           value: tr.cells.item(1).innerText,
+        }
+    }));
+    console.log(data);
+
+
 
     browser.close()
 
