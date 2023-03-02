9d8d41d1e127a85e9a644ac1d6945a3bf00a0dc9
Miguel Camarena <migcam@Miguels-MacBook-Air.local>
2023-03-02 16:24:59 -0400

    using efficient html parser

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 import { FetchRawHtml } from './fetch.js'
-import { JSDOM } from 'jsdom';
 import { parse } from 'node-html-parser';
 
 (async () => {
     const res = await FetchRawHtml('130334269')
-    const document = new JSDOM(res).window.document
-    // const document = parse(res)
+    const document = parse(res)
 
-    let selector = '#ctl00_cphMain_dvDatosContribuyentes tbody tr';
-    // let obj = {}
-    // Array.from(document.querySelectorAll(selector)).forEach(m => 
-    //     obj[`${m.childNodes[1].textContent}`]=m.childNodes[2].textContent
-    // )
-    // console.log(obj)
+    let selector = 'tr';
 
     let rnc = Array.from(document.querySelectorAll(selector)).reduce((acum,curr) => 
         Object.assign(acum,{
