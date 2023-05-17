3210953a96e10431c56ce7859e9cfd4df7ba6074
Miguel Camarena <migcam@Miguels-MacBook-Air.local>
2023-05-17 12:28:48 -0400

    add GetActualExchangeRate

---
+++ src/BCRD/BCRD.ts
@@ -0,0 +1,49 @@
+import axios from 'axios'
+import { wrapper } from 'axios-cookiejar-support'
+import { CookieJar } from 'tough-cookie';
+
+export class BCRD {
+    public static async GetTodayUSDExchangeRate(){
+        let rawRate = await this.GetActualExchangeRate();
+        return rawRate.actualPurchaseValue
+    }
+
+    public static async GetActualExchangeRate() : Promise<GetActualExchangeRateResult>{
+        //TODO: refactor
+        let jar = new CookieJar();
+        let client = wrapper(axios.create({ jar }));
+        
+        await client.get('https://www.bancentral.gov.do/SectorExterno/HistoricoTasas')
+
+        let response = await client.get('https://www.bancentral.gov.do/Home/GetActualExchangeRate')
+
+        return (response.data as BcrdResponse).result;
+
+    }
+}
+
+export interface BcrdResponse {
+    result: GetActualExchangeRateResult
+    targetUrl: any
+    success: boolean
+    error: any
+    unAuthorizedRequest: boolean
+    __abp: boolean
+}
+  
+export interface GetActualExchangeRateResult {
+    actualPurchaseValue: number
+    actualPurchaseInteranualValue: number
+    actualPurchaseAccumulatedValue: number
+    actualSellingValue: number
+    actualSellingInteranualValue: number
+    actualSellingAccumulatedValue: number
+    date: Date
+    actualPurchaseInteranualValueFormatted: string
+    actualPurchaseAccumulatedValueFormatted: string
+    actualPurchaseValueFormatted: string
+    actualSellingInteranualValueFormatted: string
+    actualSellingAccumulatedValueFormatted: string
+    actualSellingValueFormatted: string
+}
+  
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import { BCRD } from "./BCRD/BCRD";
 import { DGII } from "./DGII/DGII";
 
-
 (async () => {
-    let rnc ='130334269'
-    let company = await DGII.GetCompanyFromRNC(rnc)
-    console.log(company)
+    // let rnc ='130334269'
+    // let company = await DGII.GetCompanyFromRNC(rnc)
+    // console.log(company)
+    let rate = await BCRD.GetTodayUSDExchangeRate();
+    console.log(rate)
 })();
